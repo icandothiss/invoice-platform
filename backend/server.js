5c9f69efe1d2e74a815a2f9913292b7415cfa536
oussama othmaine <100086138+icandothiss@users.noreply.github.com>
2023-03-26 15:55:26 +0100

    deployment error

--- backend/server.js
+++ backend/server.js
@@ -12,22 +12,6 @@ connectDB();
 
 const app = express();
 
-// Dev loggin middleware
-// Serve Frontend
-if (process.env.NODE_ENV === "production") {
-  // Set build folder as static
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  // FIX: below code fixes app crashing on refresh in deployment
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-  });
-} else {
-  app.get("/", (_, res) => {
-    res.status(200).json({ message: "Welcome to the Invoice Platform" });
-  });
-}
-
 app.use(express.json());
 
 // Cookie parser
